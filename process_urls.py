e452bee7b2babeec427a84e82ee3b4046f242bfc
Eric Rasche <rasche.eric@gmail.com>
2015-10-20 22:41:05 -0500

    Update to have html output

--- process_urls.py
+++ process_urls.py
@@ -6,14 +6,23 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
 
+
 with open(sys.argv[1], 'r') as handle:
+    print """<!DOCTYPE html><html><head><title>Galaxy Package
+    Cache</title></head><body><h1>About</h1><p>This package cache serves to
+    preserve packages permanently. Please see our <a
+    href="https://github/...">Github Repository</a> for more
+    information.</p><h1>Cached
+    URLs</h1><table><thead><tr><th>sha256sum</th><th>URL</th><th>Comment</th></tr></thead><tbody>"""
+    retcode = 0
     for line in handle:
         if line.startswith('#'):
             continue
 
         data = line.strip().split('\t')
-        sha = data[0]
-        url = data[1]
+        (sha, url) = data[0:2]
+        comment = data[2] if len(data) > 2 else ""
+        print """<tr><td>{sha}</td><td><a href="{sha}">Link</a></td><td>{comment}</td></tr>""".format(sha=sha, url=url, comment=comment)
         if os.path.exists(sha):
             log.info("URL exists %s", url)
         else:
@@ -29,3 +38,5 @@ with open(sys.argv[1], 'r') as handle:
             except subprocess.CalledProcessError:
                 log.error("File has bad hash! Refusing to serve this to end users.")
                 os.unlink(sha)
+    print "</tbody></table></body></html>"
+    sys.exit(retcode)

7ad2842eefdd98b53a5e81ca1c20e2104cdc2506
Eric Rasche <rasche.eric@gmail.com>
2015-10-21 06:33:34 -0500

    Compatability with package-cache

--- gsl.py
+++ gsl.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
-
 from urlparse import urlparse
 import urllib
 import urllib2
 import click
 import os
 import hashlib
+PACKAGE_SERVER = 'https://server-to-be-determined/'
+
 
 @click.command()
 @click.option('--package_id', help='Package ID', required=True)
@@ -13,15 +14,17 @@ import hashlib
               help='Location for the downloaded file')
 def get(package_id, download_location):
     package_found = False
-    for line in urllib2.urlopen('https://github.com/bgruening/gsl/raw/master/packages.list'):
+    for line in urllib2.urlopen(PACKAGE_SERVER + 'urls.tsv'):
         if line.strip() and not line.startswith('#'):
-            iid, url, checksum = line.split('\t')
+            iid, upstream_url, checksum = line.split('\t')
             if iid.strip() == package_id.strip():
                 package_found = True
-                pkg_name = urlparse(url).path.split('/')[-1]
+                # I worry about this being unreliable. TODO: add target filename column?
+                pkg_name = urlparse(upstream_url).path.split('/')[-1]
                 storage_path = os.path.join(download_location, pkg_name)
-                urllib.urlretrieve (url, storage_path)
-                download_checksum = hashlib.sha256( open(storage_path, 'rb').read() ).hexdigest()
+                url = PACKAGE_SERVER + checksum
+                urllib.urlretrieve(url, storage_path)
+                download_checksum = hashlib.sha256(open(storage_path, 'rb').read()).hexdigest()
                 if checksum.strip() != download_checksum:
                     print 'Checksum does not match, something seems to be wrong.\n'
                     print checksum, '\t(expected)'

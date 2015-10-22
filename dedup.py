436f9f70ddccdf035681472e01b0229c6c9778e5
Eric Rasche <rasche.eric@gmail.com>
2015-10-22 16:27:35 -0500

    Small script to deduplicate URL list

---
+++ dedup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger()
+
+
+with open(sys.argv[1], 'r') as handle:
+    print '# ' + '\t'.join(['Id', 'Platform', 'Architecture', 'Upstream url', 'sha256sum', 'Alternate Url']),
+    retcode = 0
+    res = {}
+    warnings = []
+    for line in handle:
+        if line.startswith('#'):
+            continue
+
+        data = line.split('\t')
+        id = data[0]
+
+        if id not in res:
+            res[id] = []
+
+        res[id].append(data)
+
+    for x in res:
+        print '\t'.join(res[x][0]),
+
+    sys.exit(retcode)

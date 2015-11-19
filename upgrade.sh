c86a64ff0982bdbff47ac0f5b7b923d4418b5218
Eric Rasche <rasche.eric@gmail.com>
2015-11-19 12:09:07 -0600

    More debugging

--- upgrade.sh
+++ upgrade.sh
@@ -4,12 +4,14 @@
 #cat urls.tsv | awk -F'\t' '(NR>1){print $6"  "$1"-"$2"-"$3"-"$4}' > cpc.sha256sum
 
 # Renaming files into folders.
-cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4".tar.gz"; print "mkdir -p "$1"; mv "$6" "$1"/"a}' > mkdir.txt
+echo 'set -x' > mkdir.txt
+cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4".tar.gz"; print "mkdir -p "$1"; mv "$6" "$1"/"a}' >> mkdir.txt
 cat mkdir.txt | bash
 
 # Remove existing SHA256SUMs files.
 find . -name 'SHA256SUMS' -exec rm '{}' \;
 
 # Update the individual files.
-cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4".tar.gz"; print "echo "$6" >> "$1"/SHA256SUMS"}' > mv.txt
+echo 'set -x' > mv.txt
+cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4".tar.gz"; print "echo "$6" >> "$1"/SHA256SUMS"}' >> mv.txt
 cat mv.txt | bash

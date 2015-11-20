44eea0a46576652dfaf81edfac70601ee256f9fc
Eric Rasche <rasche.eric@gmail.com>
2015-11-20 14:06:37 -0600

    Prepare for cleanup from now unlinked files

--- upgrade.sh
+++ upgrade.sh
@@ -13,3 +13,6 @@ find . -name 'SHA256SUMS' -exec rm '{}' \;
 
 # Update the individual files.
 cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4".tar.gz"; print "echo \""$7"  \""a" >> "$1"/SHA256SUMS"}' | bash
+
+# Remove empty directories
+# find . -type d -empty -not -wholename '*/.git/*' -exec rmdir '{}' \;

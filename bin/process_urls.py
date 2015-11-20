e6f72e225ce760bdad921f46b149bb3e4514e8a2
Eric Rasche <rasche.eric@gmail.com>
2015-11-20 14:17:49 -0600

    Correct title

--- bin/process_urls.py
+++ bin/process_urls.py
@@ -31,7 +31,7 @@ HTML_TPL_HEAD = """
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Galaxy Package Cache</title>
+        <title>Community Package Cache</title>
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
     </head>
@@ -45,7 +45,7 @@ HTML_TPL_HEAD = """
           ga('send', 'pageview');
         </script>
         <div class="container">
-        <h1>Galaxy Package Cache</h1>
+        <h1>Community Package Cache</h1>
         <p>
             This package cache serves to preserve packages permanently. Please
             see our <a href="https://github.com/erasche/community-package-cache">Github Repository</a>

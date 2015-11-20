aa9012e6ad57128f91404d639832a5ccc0fdda70
Eric Rasche <rasche.eric@gmail.com>
2015-11-20 12:57:01 -0600

    Try symlinking in the upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -ex
 # Cleanup old hashes
 rm -f *.sha256sum
 
@@ -13,3 +13,5 @@ find . -name 'SHA256SUMS' -exec rm '{}' \;
 
 # Update the individual files.
 cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4".tar.gz"; print "echo \""$7"  \""a" >> "$1"/SHA256SUMS"}' | bash
+
+ln -s /srv/nginx/depot.galaxyproject.org/root/package/darwin/x86_64/meme/meme-4.10.0_4-Darwin-x86_64.tar.gz /srv/nginx/depot.galaxyproject.org/root/software/meme/meme_4.10.0.4_darwin_x64.tar.gz

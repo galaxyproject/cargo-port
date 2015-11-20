efb983a7270a8b74c23fd488d665ad1c28fbd8c1
Eric Rasche <rasche.eric@gmail.com>
2015-11-20 12:36:38 -0600

    Add script for collecting logs

---
+++ bin/stats.sh
@@ -0,0 +1,7 @@
+mkdir -p logs;
+cat /var/log/nginx/depot.galaxyproject.org_access.log |
+    egrep ' /software/[^/ ]+/[^/ ]+'  |
+    awk '{print $4$5" "$7}' |
+    sed 's/:/ /;s/\[//g;s/\]//g;s|/| |;s|/| |' |
+    awk '{logfile="logs/"$3"-"$2"-"$1".log"; logstmt=$4" "$5; print "grep -c \""logstmt"\" "logfile"; if [[ $? != \"0\" ]]; then echo \""logstmt"\" > "logfile"; fi;"}' |
+    bash;

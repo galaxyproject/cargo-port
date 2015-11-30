e2e3612cbe289622135f82f5c31c7cc25e1d3a6a
Eric Rasche <rasche.eric@gmail.com>
2015-11-30 10:52:06 -0600

    Update whitelist name

--- bin/process_urls.py
+++ bin/process_urls.py
@@ -183,7 +183,7 @@ def main(galaxy_package_file):
     # we need to check for files which shouldn't be there (aka things NOT
     # mentioned in urls.tsv) and remove those.
     whitelist = [
-        'SHA256SUMS', 'index.html', 'report.xml', 'api.json', 'cpc-plain-small.png', 'cpc-base.png', 'cpc-base.svg'
+        'SHA256SUM.txt', 'index.html', 'report.xml', 'api.json', 'cpc-plain-small.png', 'cpc-base.png', 'cpc-base.svg'
     ]
     for root, dirnames, filenames in os.walk('.'):
         if '.git' in root:

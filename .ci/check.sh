a1f481b1b42a1418d33c47252f7a72d87ac0df19
Eric Rasche <rasche.eric@gmail.com>
2015-12-19 10:58:00 -0600

    Add new sha256sum checker

---
+++ .ci/check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+GIT_REMOTE=$(git remote -v | grep origin | grep fetch | sed 's/^origin\s*//g;s/ .*//g')
+# Add a second remote since we can't seem to fetch master normally
+git remote add origin2 $GIT_REMOTE
+# Fetch from it
+git fetch origin2
+# And diff against that
+git diff origin2/master urls.tsv | grep -v '^+++' | grep '^+' | awk -F'\t' '{print "queryHash=$(curl -L --silent "$5" | sha256sum - | sed \"s/  .*//g\" ); userHash="$7"; if [ \"$queryHash\" == \"$userHash\" ]; then echo \"Valid hash\"; else exit 1; fi;"}' | bash

0baaf97b2609d55bbf8f25785708560389c51501
Eric Rasche <rasche.eric@gmail.com>
2016-02-25 20:23:32 +0000

    Fixes #51

--- upgrade.sh
+++ upgrade.sh
@@ -12,7 +12,7 @@ cat urls.tsv | awk -F'\t' '(NR>1){print "mkdir -p "$1";"}' | bash
 find . -name 'SHA256SUMS' -exec rm '{}' \;
 
 # Update the individual files.
-cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4".tar.gz"; print "echo \""$7"  \""a" >> "$1"/SHA256SUM.txt"}' | bash
+cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4$6; print "echo \""$7"  \""a" >> "$1"/SHA256SUM.txt"}' | bash
 
 # Remove empty directories
 #find . -type d -empty -not -wholename '*/.git/*' -exec rmdir '{}' \;

61bf6f599d59df38ccb9920ac0faff17393439ba
Eric Rasche <rasche.eric@gmail.com>
2016-08-11 14:20:01 +0000

    Fixes #85

---
+++ bin/travis.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sys
+import logging
+from cargoport.utils import yield_packages, download_url, package_to_path, verify_file
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger()
+
+
+def main():
+    for package in yield_packages(sys.stdin):
+        print package
+        # Remove the '+' at the beginning
+        package['id'] = package['id'][1:]
+
+        output_package_path = package_to_path(**package) + package['ext']
+        err = download_url(package['url'], output_package_path)
+
+        if err is not None:
+            log.error("Could not download file", err)
+
+        err = verify_file(output_package_path, package['sha'])
+
+        if err is not None:
+            log.error("Could not verify file", err)
+
+
+if __name__ == '__main__':
+    main()
--- cargoport/utils.py
+++ cargoport/utils.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+import os
+import subprocess
 import logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
 HEADER_KEYS = ['id', 'version', 'platform', 'arch', 'url', 'ext', 'sha256sum', 'upstream_first']
 PACKAGE_SERVER = 'https://depot.galaxyproject.org/software/'
+
 
 def yield_packages(handle, meta=False, retcode=None):
     for lineno, line in enumerate(handle):
@@ -25,14 +28,44 @@ def yield_packages(handle, meta=False, retcode=None):
         except Exception, e:
             log.error(str(e))
 
+
 def package_name(ld):
     return '_'.join(ld[key] for key in HEADER_KEYS[0:4]) + ld['ext']
 
+
 def depot_url(ld):
     return PACKAGE_SERVER + '{id}/{id}_{version}_{platform}_{arch}{ext}'.format(**ld)
+
 
 def get_url(ld):
     if ld['upstream_first'] == 'True':
         return ld['url']
     else:
         return depot_url(ld)
+
+
+def download_url(url, output):
+    try:
+        args = ['curl', '-L', '-k', '--max-time', '720']
+
+        args += [url, '-o', output]
+        subprocess.check_call(args)
+    except subprocess.CalledProcessError, cpe:
+        log.error("File not found")
+        return str(cpe)
+
+
+def package_to_path(id="", version="", platform="", arch="", ext="", **kwargs):
+    return '_'.join([id, version, platform, arch])
+
+
+def verify_file(path, sha):
+    try:
+        filehash = subprocess.check_output(['sha256sum', path])[0:64].strip()
+        if filehash.lower() != sha.lower():
+            excstr = "Bad hash, %s != %s in %s" % (filehash.lower(), sha.lower(), path)
+            raise Exception(excstr)
+    except Exception, cpe:
+        log.error("File has bad hash! Removing from disk")
+        os.unlink(path)
+        return str(cpe)

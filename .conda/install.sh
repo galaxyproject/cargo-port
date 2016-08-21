3ae9a8d08d1ad65ac678c3d823bed4bb81769133
Eric Rasche <rasche.eric@gmail.com>
2016-08-21 18:20:11 +0000

    One off

--- .conda/install.sh
+++ .conda/install.sh
@@ -7,7 +7,9 @@ else
     echo "Installing Conda"
     curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
     bash Miniconda2-latest-Linux-x86_64.sh -b -p $CONDA_INSTALLATION_PATH || echo "Conda already installed"
+    conda install -y conda-build
 fi
+conda install -y conda-build
 
 if hash conda 2>/dev/null; then
     echo "Conda build already installed"

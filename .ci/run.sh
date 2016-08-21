c11a66acd3f2736b7d214f6bd04191233d9337da
Eric Rasche <rasche.eric@gmail.com>
2016-08-21 18:02:22 +0000

    Run conda code

--- .ci/run.sh
+++ .ci/run.sh
@@ -7,6 +7,12 @@ rsync -avr . $CPC_HOST:$CPC_DIR/
 ssh $CPC_HOST "cd $CPC_DIR && sh upgrade.sh"
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls.tsv > api-tcp.json"
 rsync $CPC_HOST:$CPC_DIR/report.xml report-tcp.xml
+
+bash .conda/run.sh
+
+# Update now taht we have a urls-bioconda
+rsync -avr urls-bioconda.tsv $CPC_HOST:$CPC_DIR/urls-bioconda.tsv
+
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls-bioconda.tsv > api-bioconda.json"
 rsync $CPC_HOST:$CPC_DIR/report.xml report-bioconda.xml
 ssh $CPC_HOST "cd $CPC_DIR && python bin/merge_apis.py api-tcp.json tcp api-bioconda.json bioconda > api.json"

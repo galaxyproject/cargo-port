c5871e0c17b24e6e8642d9b7e10012fc3535085c
Eric Rasche <rasche.eric@gmail.com>
2016-08-21 18:04:08 +0000

    Echo logs instead

--- .conda/install.sh
+++ .conda/install.sh
@@ -6,7 +6,7 @@ CONDA_INSTALLATION_PATH=/tmp/anaconda
 if hash conda 2>/dev/null; then
     echo "Conda already installed"
 else
-    date "Installing Conda"
+    echo "Installing Conda"
     curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
     bash Miniconda2-latest-Linux-x86_64.sh -b -p CONDA_INSTALLATION_PATH/anaconda
     export PATH=CONDA_INSTALLATION_PATH/bin:$PATH
@@ -15,7 +15,7 @@ fi
 if hash conda 2>/dev/null; then
     echo "Conda build already installed"
 else
-    date "Installing conda-build"
+    echo "Installing conda-build"
     conda install -y conda-build
 fi
 

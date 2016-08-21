9323081e8db29d5152a24d72de7a9001c5d59886
Eric Rasche <rasche.eric@gmail.com>
2016-08-21 18:23:07 +0000

    Don't send everything

--- .ci/run.sh
+++ .ci/run.sh
@@ -3,7 +3,7 @@ CPC_HOST=depot@orval.galaxyproject.org
 CPC_DIR=/srv/nginx/depot.galaxyproject.org/root/software
 rm -f report*.xml
 
-rsync -avr . $CPC_HOST:$CPC_DIR/
+rsync -avr --exclude bioconda-recipes --exclude .git . $CPC_HOST:$CPC_DIR/
 
 ssh $CPC_HOST "cd $CPC_DIR && sh upgrade.sh"
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls.tsv > api-tcp.json"

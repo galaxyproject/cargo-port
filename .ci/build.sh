9d42d7cf453877e94bb3666021fc126de71cfa25
Eric Rasche <rasche.eric@gmail.com>
2016-08-21 17:23:42 +0000

    Enable conda fetcher to run

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+## Cargo port
 git fetch master
 git diff master
 python setup.py build
 python setup.py install
 galaxy-cachefile-validator urls.tsv
+
+## Bioconda
+bash .conda/run.sh

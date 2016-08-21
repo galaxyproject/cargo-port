0e231bea3f44ee82c5ee531605419b56294d1549
Eric Rasche <rasche.eric@gmail.com>
2016-08-21 17:53:20 +0000

    Bioconda not bioblend

--- .ci/run.sh
+++ .ci/run.sh
@@ -6,7 +6,7 @@ rsync -avr . $CPC_HOST:$CPC_DIR/
 
 ssh $CPC_HOST "cd $CPC_DIR && sh upgrade.sh"
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls.tsv > api-tcp.json"
-ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls-bioblend.tsv > api-bioblend.json"
-ssh $CPC_HOST "cd $CPC_DIR && python bin/merge_apis.py api-tcp.json tcp api-bioblend.json bioblend > api-bioblend.json"
-
-rsync $CPC_HOST:$CPC_DIR/report.xml .
+rsync $CPC_HOST:$CPC_DIR/report.xml report-tcp.xml
+ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls-bioconda.tsv > api-bioconda.json"
+rsync $CPC_HOST:$CPC_DIR/report.xml report-bioconda.xml
+ssh $CPC_HOST "cd $CPC_DIR && python bin/merge_apis.py api-tcp.json tcp api-bioconda.json bioconda > api-bioconda.json"

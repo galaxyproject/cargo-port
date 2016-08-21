8f69ef8290c03e2f8bcfbe792fcae26bfc255530
Eric Rasche <rasche.eric@gmail.com>
2016-08-21 17:41:23 +0000

    sha256s

--- upgrade.sh
+++ upgrade.sh
@@ -6,3 +6,10 @@ find . -name 'SHA256SUM.txt' -exec rm '{}' \;
 cat urls.tsv | awk -F'\t' '(NR>1){print "mkdir -p "$1";"}' | bash
 # Update the individual files.
 cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4$6; print "echo \""$7"  \""a" >> "$1"/SHA256SUM.txt"}' | bash
+
+if [ -e "urls-bioconda.tsv" ]; then
+    # Create appropriate folders
+    cat urls.tsv | awk -F'\t' '(NR>1){print "mkdir -p "$1";"}' | bash
+    # Update the individual files.
+    cat urls.tsv | awk -F'\t' '(NR>1){a=$1"_"$2"_"$3"_"$4$6; print "echo \""$7"  \""a" >> "$1"/SHA256SUM.txt"}' | bash
+fi

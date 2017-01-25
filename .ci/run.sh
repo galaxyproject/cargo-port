cb76008e92aade8f7271e5e3ef3648f610563920
Eric Rasche <rasche.eric@gmail.com>
2017-01-25 05:57:21 +0000

    update index sooner

--- .ci/run.sh
+++ .ci/run.sh
@@ -4,6 +4,9 @@ CPC_HOST=depot@orval.galaxyproject.org
 CPC_DIR=/srv/nginx/depot.galaxyproject.org/root/software
 rm -f report*.xml
 
+# Update the index.html from the template
+cat index.html.tpl | sed "s|BUILD_TAG_GOES_HERE|Build <a href='${BUILD_URL}'>#${BUILD_NUMBER}</a>|" > index.html
+
 rsync -avr --exclude bioconda-recipes --exclude .git . $CPC_HOST:$CPC_DIR/
 
 ssh $CPC_HOST "cd $CPC_DIR && sh upgrade.sh"
@@ -14,9 +17,6 @@ rsync $CPC_HOST:$CPC_DIR/report.xml report-tcp.xml
 bash .conda/run.sh
 # Update now that we have a urls-bioconda
 rsync -avr urls-bioconda.tsv $CPC_HOST:$CPC_DIR/urls-bioconda.tsv
-
-# Update the index.html from the template
-cat index.html.tpl | sed "s|BUILD_TAG_GOES_HERE|Build <a href='${BUILD_URL}'>#${BUILD_NUMBER}</a>|" > index.html
 
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls-bioconda.tsv > api-bioconda.json"
 rsync $CPC_HOST:$CPC_DIR/report.xml report-bioconda.xml

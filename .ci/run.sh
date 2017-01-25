384a22580f648ed5cb8384e60ffc5dde4217f06c
Eric Rasche <rasche.eric@gmail.com>
2017-01-25 05:33:38 +0000

    Template out index.html and fill from Jenkins
    Fixes #111

--- .ci/run.sh
+++ .ci/run.sh
@@ -9,9 +9,13 @@ ssh $CPC_HOST "cd $CPC_DIR && sh upgrade.sh"
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls.tsv > api-tcp.json"
 rsync $CPC_HOST:$CPC_DIR/report.xml report-tcp.xml
 
+# Run conda
 bash .conda/run.sh
 # Update now that we have a urls-bioconda
 rsync -avr urls-bioconda.tsv $CPC_HOST:$CPC_DIR/urls-bioconda.tsv
+
+# Update the index.html from the template
+cat index.html.tpl | sed "s|BUILD_TAG_GOES_HERE|Build <a href='${BUILD_URL}'>#${BUILD_NUMBER}</a>|" > index.html
 
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls-bioconda.tsv > api-bioconda.json"
 rsync $CPC_HOST:$CPC_DIR/report.xml report-bioconda.xml

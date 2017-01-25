2e1733ff842889a6eb2430284462c3e9ecfbe440
Eric Rasche <rasche.eric@gmail.com>
2017-01-25 05:39:34 +0000

    Logging hurts no-one

--- .conda/get_meta.sh
+++ .conda/get_meta.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-
+set -ex
 
 DIRECTORY=./bioconda-recipes
 
 if [ -d "$DIRECTORY" ]; then
     cd "$DIRECTORY"
-    git pull origin master > /dev/null
+    git pull origin master
 else
-    git clone --quiet https://github.com/bioconda/bioconda-recipes.git
+    git clone https://github.com/bioconda/bioconda-recipes.git
     cd bioconda-recipes
 fi
 git log --name-only --pretty="" --since="2 months ago" | grep -E '^recipes/.*/meta.yaml'

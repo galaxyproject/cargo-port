245ecf7952cc78b95133159f5dc8adb9b171917d
Eric Rasche <rasche.eric@gmail.com>
2017-01-25 06:05:29 +0000

    Don't clobber, docs

--- .ci/run.sh
+++ .ci/run.sh
@@ -7,9 +7,13 @@ rm -f report*.xml
 # Update the index.html from the template
 cat index.html.tpl | sed "s|BUILD_TAG_GOES_HERE|Build <a href='${BUILD_URL}'>#${BUILD_NUMBER}</a>|" > index.html
 
+# Copy over all changed files to the target
 rsync -avr --exclude bioconda-recipes --exclude .git . $CPC_HOST:$CPC_DIR/
 
+# Any one-time upgrades
 ssh $CPC_HOST "cd $CPC_DIR && sh upgrade.sh"
+
+# Process URLs and copy back report
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls.tsv > api-tcp.json"
 rsync $CPC_HOST:$CPC_DIR/report.xml report-tcp.xml
 
@@ -18,6 +22,9 @@ bash .conda/run.sh
 # Update now that we have a urls-bioconda
 rsync -avr urls-bioconda.tsv $CPC_HOST:$CPC_DIR/urls-bioconda.tsv
 
+# Process URLs and copy back report
 ssh $CPC_HOST "cd $CPC_DIR && python bin/process_urls.py urls-bioconda.tsv > api-bioconda.json"
 rsync $CPC_HOST:$CPC_DIR/report.xml report-bioconda.xml
+
+# Merge the APIs on the remote end. Allows us to see both TCP + bioconda packages together.
 ssh $CPC_HOST "cd $CPC_DIR && python bin/merge_apis.py api-tcp.json tcp api-bioconda.json bioconda > api.json"
--- .conda/install.sh
+++ .conda/install.sh
@@ -5,7 +5,7 @@ if hash conda 2>/dev/null; then
     echo "Conda already installed"
 else
     echo "Installing Conda"
-    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+    wget --no-clobber --continue https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
     bash Miniconda2-latest-Linux-x86_64.sh -b -p $CONDA_INSTALLATION_PATH || echo "Conda already installed"
     echo "Installing conda-build"
     conda install -y conda-build

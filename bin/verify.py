2f4cabaf9219216149af6b4edea74ba2dec6f976
E Rasche <hxr@hx42.org>
2017-06-04 21:51:18 +0000

    fix newline

--- bin/verify.py
+++ bin/verify.py
@@ -99,7 +99,7 @@ def verify_file(path, sha, dryrun=False):
 
 
 def verify_filetype(path, ext, dryrun=False):
-    mimetype = subprocess.check_output(['file', path])
+    mimetype = subprocess.check_output(['file', path]).strip()
     log.info("Mimetype of %s is %s", path, mimetype)
     # Currently just passing on without error.
     return

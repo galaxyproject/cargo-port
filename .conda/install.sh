9ea610e1a288dd92cb620e7e2afb38bd6bedc449
Helena Rasche <hxr@hx42.org>
2019-03-01 09:26:24 +0100

    add some echos

--- .conda/install.sh
+++ .conda/install.sh
@@ -4,9 +4,8 @@ set -ex
 if hash conda 2>/dev/null; then
     echo "Conda already installed"
 else
-    echo "Installing Conda"
     wget --no-clobber --continue https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
     bash Miniconda2-latest-Linux-x86_64.sh -b -p $CONDA_INSTALLATION_PATH || echo "Conda already installed"
-    echo "Installing conda-build"
+    echo $PATH $CONDA_INSTALLATION_PATH
     conda install -y conda-build
 fi

e547a8fff14a7c4461dcdf8f82b758cb9fbff76e
Bjoern Gruening <bjoern.gruening@gmail.com>
2020-04-07 19:18:07 +0200

    use curl

--- .conda/install.sh
+++ .conda/install.sh
@@ -5,8 +5,9 @@ if hash conda 2>/dev/null; then
     echo "Conda already installed"
 else
     rm -rf $CONDA_INSTALLATION_PATH
-    wget --no-clobber --continue https://repo.anaconda.com/miniconda/${CONDA_VERSION_TO_INSTALL}-Linux-x86_64.sh
-    bash ${CONDA_VERSION_TO_INSTALL}-Linux-x86_64.sh -b -p $CONDA_INSTALLATION_PATH || echo "Conda already installed"
+    ##wget --no-clobber --continue https://repo.anaconda.com/miniconda/${CONDA_VERSION_TO_INSTALL}-Linux-x86_64.sh
+    curl -s https://repo.anaconda.com/miniconda/${CONDA_VERSION_TO_INSTALL}-Linux-x86_64.sh --output Miniconda.sh
+    bash Miniconda.sh -b -p $CONDA_INSTALLATION_PATH || echo "Conda already installed"
     echo $PATH $CONDA_INSTALLATION_PATH
     conda install -y conda-build
 fi
